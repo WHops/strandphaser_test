b3de18473efdb342e17e9c23fae2f8033d9bbdc6
Wolfram Höps <wolfram.hoeps@yahoo.de>
2023-02-28 10:42:48 +0100

    Updated code

--- R/make_phase_plots.R
+++ R/make_phase_plots.R
@@ -1,13 +1,40 @@
 # Comparing StrandPhaseR output
 
-full_res_link = '/Users/hoeps/PhD/hack_23/strandphaser/full_res/strand_states/fullres_finals.txt'
-pool_res_link = '/Users/hoeps/PhD/hack_23/strandphaser/pool_res/StrandPhaseR_final_output.txt'
+library(ggplot2)
+library(dplyr)
+
+full_res_link = '../data/fullres_finals.txt'
+pool_res_link = '../data/StrandPhaseR_final_output.txt'
 
 
-fr = read.table(full_res_link, header=T, sep='\t')
-pr = read.table(pool_res_link, header=T, sep='\t')
+fullres = read.table(full_res_link, header=T, sep='\t')
+poolres = read.table(pool_res_link, header=T, sep='\t')
 
-cell = pr$cell[1]
+fullres$source = 0
+poolres$source = 0.3
 
-frsub = fr[fr$cell == cell,]
-prsub = pr[pr$cell == cell,]
+cells = poolres$cell
+
+for (cell in cells){
+  
+
+fullres_cell = fullres[fullres$cell == cell,]
+poolres_cell = poolres[poolres$cell == cell,]
+
+mixedres_cell = rbind(fullres_cell, poolres_cell)
+
+mixedres_cell[mixedres_cell$chrom == 'chrX', 'chrom'] = 'chr23'
+mixedres_cell[mixedres_cell$chrom == 'chrY', 'chrom'] = 'chr24'
+
+mixedres_cell$y_position = as.numeric(sub("chr", "", mixedres_cell$chrom))
+
+plot = ggplot(mixedres_cell) + geom_segment(aes(x=start, 
+                                         xend = end, 
+                                         y= - (y_position + source), 
+                                         yend =  - (y_position + source), 
+                                         color=class),
+                                     size=2) + 
+  labs(x='position[bp]', y='chromsome', title=paste0(cell, ' (top: full; bottom: pooled)')) + theme_bw()
+
+ggsave(file = paste0('../plots/', cell, '.pdf'), plot, width=16, height = 16, units = 'cm', device='pdf' )
+}
